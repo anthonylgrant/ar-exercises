24e7abfc1b5d409ec43a1184079d3e47a33607fa
Anthony Grant <anthonylorenzogrant@gmail.com>
2016-12-06 20:51:49 +0000

    finished first and second excercises, removed instance variable from 1st exercise after learning create method changes data persistently

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,41 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+
 # Your code goes below here ...
+# class Store < ApplicationRecord
+# end
+
+Store.create([
+  {
+    name: "Burnaby",
+    annual_revenue: 300000,
+    mens_apparel: true,
+    womens_apparel: true,
+  },
+  {
+    name: "Burnaby",
+    annual_revenue: 1260000,
+    mens_apparel: false,
+    womens_apparel: true,
+  },
+  {
+    name: "Gastown",
+    annual_revenue: 190000,
+    mens_apparel: true,
+    womens_apparel: false,
+  }
+])
+
+pp Store.all
+pp Store.count
+
+# CREATE TABLE "stores" (
+# "id" serial primary key,
+# "name" character varying,
+# "annual_revenue" integer,
+# "mens_apparel" boolean,
+# "womens_apparel" boolean,
+# "created_at" timestamp NOT NULL,
+# "updated_at" timestamp NOT NULL
+# )
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+pp @store1
+pp @store2
+
+pp @store2.name = "Coolcitymang"
