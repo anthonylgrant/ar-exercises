cf140552a43d90bd0276440f18392b0276d1a878
Anthony Grant <anthonylorenzogrant@gmail.com>
2016-12-06 22:24:11 +0000

    created 2 employee instances, finished exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,30 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  {
+    first_name: "Khurram",
+    last_name: "Virani",
+    hourly_rate: 60
+  },
+  {
+    first_name: "Don",
+    last_name: "Burls",
+    hourly_rate: 70
+  }
+])
+
+@store2.employees.create([
+  {
+    first_name: "Anthony",
+    last_name: "Grant",
+    hourly_rate: 100
+  },
+  {
+    first_name: "Josh",
+    last_name: "Coles",
+    hourly_rate: 20
+  }
+])
+
+pp Employee.all
